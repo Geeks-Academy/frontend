2ebef982d6b275ea189f7682f49056889d9aff57
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-08-20 16:10:22 +0200

    FRDQ-4 - configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
-    extends: [
-        "airbnb",
-        "airbnb/hooks"
+  extends: ['airbnb', 'airbnb/hooks', 'prettier', 'prettier/react'],
+  env: {
+    jest: true,
+    browser: true,
+  },
+  globals: {
+    document: true,
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
     ],
-    env: {
-        jest: true,
-        browser: true
-    },
-    globals: {
-        document: true,
-    },
-    rules: {
-        "react/jsx-props-no-spreading": 0,
-        "react/jsx-filename-extension": [
-            1,
-            {
-                "extensions": [
-                    ".js"
-                ]
-            }
-        ],
-    }
-}
+  },
+};
