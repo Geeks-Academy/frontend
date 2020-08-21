b56933a440a3c8642ac05a60f8315b10993c2df5
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-08-21 09:43:05 +0200

    Implement global typography

---
+++ src/shared/styles/typography.js
@@ -0,0 +1,68 @@
+export const globalStyles = `
+	font-family: Lato;
+	font-style: normal;
+	color: #000000;
+`;
+export const headerXxl = `	font-weight: normal;
+	font-size: 37px;
+  line-height: 135%;`;
+
+export const headerXl = `	font-weight: normal;
+	font-size: 33px;
+  line-height: 135%;`;
+
+export const headerL = `	font-weight: normal;
+	font-size: 29px;
+  line-height: 135%;`;
+
+export const headerM = `	font-weight: normal;
+	font-size: 25px;
+  line-height: 135%;`;
+
+export const headerS = `	font-weight: normal;
+	font-size: 21px;
+  line-height: 135%;`;
+
+export const bodyL = `	font-weight: normal;
+	font-size: 17px;
+  line-height: 140%;`;
+
+export const bodyM = `	font-weight: normal;
+	font-size: 15px;
+  line-height: 135%;`;
+
+export const bodyS = `	font-weight: normal;
+	font-size: 13px;
+  line-height: 135%;`;
+
+export const headerXxlBold = `	font-weight: bold;
+	font-size: 37px;
+  line-height: 135%;`;
+
+export const headerXlBold = `	font-weight: bold;
+	font-size: 33px;
+  line-height: 135%;`;
+
+export const headerLBold = `	font-weight: bold;
+	font-size: 29px;
+  line-height: 135%;`;
+
+export const headerMBold = `	font-weight: bold;
+	font-size: 25px;
+  line-height: 135%;`;
+
+export const headerSBold = `	font-weight: bold;
+	font-size: 21px;
+  line-height: 135%;`;
+
+export const bodyLBold = `	font-weight: bold;
+	font-size: 17px;
+  line-height: 140%;`;
+
+export const bodyMBold = `	font-weight: bold;
+	font-size: 15px;
+  line-height: 135%;`;
+
+export const bodySBold = `	font-weight: bold;
+	font-size: 13px;
+  line-height: 135%;`;
