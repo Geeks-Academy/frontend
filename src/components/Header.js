f32aa0ca5087b61e9463631ccca1e7cd7fababf7
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-22 15:46:42 +0200

    FRDQ-15 - adds header component and media queries

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import device from '../styles/devices';
+import typography from '../styles/typography';
+import colors from '../styles/colors';
+
+const Title = styled.h1`
+  color: ${colors.text.NeutralBlack};
+  ${typography.header.bold.L} 
+  
+  @media ${device.mobile} { 
+    ${typography.header.bold.S}
+  }
+`;
+
+function Header({ title }) {
+  return <Title>{title}</Title>;
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+Header.defaultProps = {
+  title:
+    'Join our IT platform to share ideas, exchange knowledge and build a team!',
+};
+
+export default Header;
---
+++ src/styles/devices.js
@@ -0,0 +1,7 @@
+const device = {
+  mobile: `(max-width: 767px)`,
+  tablet: `(min-width: 768px) and (max-width: 991px})`,
+  desktop: `(min-width: 992)`,
+  desktopFHD: `(min-width: 1920)`,
+};
+export default device;
