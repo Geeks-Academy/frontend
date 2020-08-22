89ae5e845e0bdf6325a223b53ad7b80cb4217884
Oskar Wojciechowski <oskar.woj@gmail.com>
2020-08-22 21:03:11 +0200

    reverse React.StrictMode and SnackBar on top

--- src/components/SnackBar.js
+++ src/components/SnackBar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import { StyledAlert } from './SnackBar.styled';
 
-const SnackBar = ({ className, error, open }) => {
+const SnackBar = ({ className, open, error }) => {
   const text = error
     ? 'Error! Message after unsuccessful login'
     : 'Success! Message after successful login';
 
   return (
-    <Snackbar open={open} autoHideDuration={5000}>
+    <Snackbar
+      open={open}
+      autoHideDuration={5000}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    >
       <StyledAlert className={className} icon={false} alertcolor={error}>
         {text}
       </StyledAlert>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
