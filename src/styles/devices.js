d37f7c19ab2eae3a1ffacf122fd935ea22b13e3f
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-23 13:32:03 +0200

    FRDQ-15 device resolution breakpoints change

--- src/styles/devices.js
+++ src/styles/devices.js
@@ -1,7 +1,7 @@
 const device = {
   mobile: `@media (max-width: 767px)`,
-  tablet: `@media (min-width: 768px) and (max-width: 991px})`,
-  desktop: `@media (min-width: 992)`,
+  tablet: `@media (min-width: 768px) and (max-width: 1365px)`,
+  desktop: `@media (min-width: 1366) and (max-width: 1919px)`,
   desktopFHD: `@media (min-width: 1920)`,
 };
 export default device;
