25dd6b876fd36f9f1bedd2cdf61e41e4a7043fd4
Hayuna <marek.kowalonek@gmail.com>
2020-08-23 20:27:05 +0200

    FRDQ-29 - Refactor

--- src/components/atoms/ListItem/ListItem.tsx
+++ src/components/atoms/ListItem/ListItem.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
 import { Item, StyledLink } from './ListItem.styled';
+import { ILink, IListItem } from './ListItem.model'
 
-interface ItemProps {
-  id: number;
-  txt: string;
-  link?: null | { url: string; text: string };
+interface IProps {
+  item: IListItem;
+  icon: string;
 }
 
-const ListItem = ({ item, icon }: { item: ItemProps, icon: string }) => {
-  return <Item>
-    <img src={icon} alt="icon" />
-    {item.txt}
-    {item.link && (
-      <StyledLink to={item.link.url}>
-        {item.link.text}
-      </StyledLink>
-    )}
-  </Item>
+const ListItem = ({ item, icon }: IProps) => {
+  return (
+    <Item>
+      <img src={icon} alt="icon" />
+      {item.txt}
+      {item.link && (
+        <StyledLink to={item.link.url}>
+          {item.link.text}
+        </StyledLink>
+      )}
+    </Item>
+  )
 };
 
 export default ListItem;
