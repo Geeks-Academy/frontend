ebd277b197f8d3bbf3826c6721f3c337db75e72c
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-23 13:33:54 +0200

    FRDQ-15 import order changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
 import device from '../styles/devices';
-import typography from '../styles/typography';
 import colors from '../styles/colors';
+import typography from '../styles/typography';
 
 const Title = styled.h1`
   color: ${colors.text.NeutralBlack};
