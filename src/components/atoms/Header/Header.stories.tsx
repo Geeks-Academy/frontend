edd9c36d2e73dc96888dbf43b20bb9168504cc1b
Hayuna <marek.kowalonek@gmail.com>
2020-08-29 15:43:24 +0200

    FRDQ-41 - Update Header component, add types

--- src/components/atoms/Header/Header.stories.tsx
+++ src/components/atoms/Header/Header.stories.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Header, { IProps } from './Header';
 
 export default {
-    title: 'atoms/header',
-    component: Header,
+  title: 'atoms/header',
+  component: Header,
+  argTypes: {
+    title: {
+      defaultValue: 'Join our IT platform to share ideas, exchange knowledge and build a team!',
+      controls: {
+        type: 'text',
+      },
+    },
+  },
 };
 
-export const Default = ({ title = "Join our IT platform to share ideas, exchange knowledge and build a team!" }: IProps) => (
-    <Header title={title} />
-);
+export const Default = (props: IProps): JSX.Element => <Header {...props} />;
