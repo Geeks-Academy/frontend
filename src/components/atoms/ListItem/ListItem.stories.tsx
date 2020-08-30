35cb4f8e9f32ee8895c608f9d85996c72482bff0
Tomasz Dłuski <mr.toumash@gmail.com>
2020-08-30 02:01:31 +0200

    FRDQ-26 - Moves all args into storybook .args with ts types

--- src/components/atoms/ListItem/ListItem.stories.tsx
+++ src/components/atoms/ListItem/ListItem.stories.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
+
 import { MemoryRouter } from 'react-router-dom';
+import { Meta, Story } from '@storybook/react/types-6-0';
+
 import ListItem, { IProps } from './ListItem';
 import listItemIcon from '../../../assets/listItemIcon.svg';
 
 export default {
   title: 'atoms/ListItem',
   component: ListItem,
-  decorators: [
-    (Story: any): JSX.Element => (
-      <MemoryRouter>
-        <Story />
-      </MemoryRouter>
-    ),
-  ],
-  args: {
-    item: {
-      id: 0,
-      txt: 'Improve your skills and work on amazing IT projects',
-      link: null,
-    },
-    icon: listItemIcon,
-  },
-};
+  decorators: [(story) => <MemoryRouter>{story()}</MemoryRouter>],
+} as Meta;
 
-export const Default = (props: IProps): JSX.Element => <ListItem {...props} />;
+const Template: Story<IProps> = (props: IProps): JSX.Element => <ListItem {...props} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  item: {
+    id: 0,
+    txt: 'Improve your skills and work on amazing IT projects',
+    link: undefined,
+  },
+  icon: listItemIcon,
+};
 
 export const WithLink = Default.bind({});
 WithLink.args = {
+  ...Default.args,
   item: {
+    id: 0,
     txt: 'Find support you need and help other people',
     link: {
       url: 'https://bit.ly/3hHhnSW',
