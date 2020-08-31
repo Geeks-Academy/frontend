bce1dd3942a806c264138cae8734293cc76e8ef1
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-08-31 15:52:02 +0200

    FRDQ-24 - Fixed SocialButton component return and Storybook

--- src/components/atoms/SocialButton/SocialButton.stories.tsx
+++ src/components/atoms/SocialButton/SocialButton.stories.tsx
@@ -1,34 +1,16 @@
-import { ReactComponent as GithubLogo } from 'assets/github-logo.svg';
-import { ReactComponent as GoogleLogo } from 'assets/google-logo.svg';
 import React from 'react';
-import SocialButton, { IProps } from './SocialButton';
+import SocialButton from './SocialButton';
 import { Logo, Role } from './SocialButton.model';
 
 export default {
   title: 'atoms/SocialButton',
   component: SocialButton,
-  args: {
-    roleDependText: Role.DEVELOPER || Role.NONDEVELOPER,
-    logo: GoogleLogo || GithubLogo,
-  },
-  argTypes: {
-    roleDependText: {
-      control: {
-        type: 'text',
-      },
-    },
-    logo: {
-      control: {
-        type: Logo,
-      },
-    },
-  },
 };
 
-export const GoogleButton = ({ logo, roleDependText }: IProps): JSX.Element => (
-  <SocialButton logo={logo} roleDependText={roleDependText} />
+export const GoogleButton = (): JSX.Element => (
+  <SocialButton logo={Logo.GOOGLE} roleDependText={Role.NONDEVELOPER} />
 );
 
-export const GithubButton = ({ logo, roleDependText }: IProps): JSX.Element => (
-  <SocialButton logo={logo} roleDependText={roleDependText} />
+export const GithubButton = (): JSX.Element => (
+  <SocialButton logo={Logo.GITHUB} roleDependText={Role.DEVELOPER} />
 );
