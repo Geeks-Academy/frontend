1259f9a5fd9a830e2dd01c9ff5bead46aec7e71d
Tomasz Korenberg <info@casestudio.pl>
2020-09-05 22:12:40 +0200

    FRDQ-71 - Store all assets in one file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as HomePageImg_1 } from './homepage-image-1.svg';
 export { default as listItemIcon } from './listItemIcon.svg';
+export { default as ButtonIcon } from './button-icon.svg';
+export { default as GithubLogo } from './github-logo.svg';
+export { default as GoogleLogo } from './google-logo.svg';
