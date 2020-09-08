ed7982c65e78935d78744140f2c594e6c713d937
Hayuna <marek.kowalonek@gmail.com>
2020-09-08 20:15:15 +0200

    FRDQ-80 - Remove unnecessary components

--- src/components/atoms/TextArea/TextArea.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import colors from 'styles/colors';
-
-export const StyledTextArea = styled.textarea`
-  width: 300px;
-  height: 100px;
-  border: 1px solid ${colors.background.Neutral80};
-  padding: 5px;
-`;
