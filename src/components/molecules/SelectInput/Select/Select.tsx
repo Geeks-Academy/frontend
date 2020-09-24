12970a7ab1d822eafb772de8f80e4c7e4bd57540
MaciejSpalek <spwrtt@gmail.com>
2020-09-24 13:14:32 +0200

    FRDQ-12 - Add mouseEvent for closing List, remove bug related to detect input, change functions' name

--- src/components/molecules/SelectInput/Select/Select.tsx
+++ src/components/molecules/SelectInput/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { MouseEvent, useEffect, useState } from 'react';
 import { ArrowIcon } from 'assets';
 import {
   StyledArrowImage,
@@ -19,13 +19,22 @@ const Select = ({
   selectCaption,
   inputPlaceholder,
 }: ISelect): JSX.Element => {
+  const [currentOptionId, setCurrentOptionId] = useState<string>('');
   const [isSelectOpened, setIsSelectOpened] = useState<boolean>(false);
   const [selectedOptions, setSelectedOptions] = useState<ISingleOption[]>([]);
-  const [currentOptionId, setCurrentOptionId] = useState<string>('');
   const [mutableOptions, setMutableOptions] = useState<ISingleOption[]>(options);
 
   const doesArrayConsistValue = (id: string) => {
     return selectedOptions.filter((element: ISingleOption) => element.id === id).length;
+  };
+
+  const isInputEmpty = (value: string) => {
+    return value === '';
+  };
+
+  const isInputTextMatch = (inputText: string, value: string) => {
+    const regex = new RegExp(`^${inputText}`, 'i');
+    return regex.test(value);
   };
 
   const updateSelectedOptions = (option: ISingleOption): void => {
@@ -43,16 +52,11 @@ const Select = ({
     }
   };
 
-  const isInputTextMatch = (inputText: string, value: string) => {
-    const regex = new RegExp(`^${inputText}`, 'i');
-    return regex.test(value);
+  const hideList = () => {
+    setIsSelectOpened(false);
   };
 
-  const isInputEmpty = (value: string) => {
-    return value === '';
-  };
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const filterOptions = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     const tempOptions: ISingleOption[] = [];
 
@@ -69,6 +73,13 @@ const Select = ({
     }
   };
 
+  const toggleList = (e: MouseEvent<HTMLDivElement>) => {
+    const clickedElementName = (e.target as HTMLDivElement).localName;
+    if (clickedElementName !== 'input') {
+      setIsSelectOpened(!isSelectOpened);
+    }
+  };
+
   useEffect(() => {
     handleOnClick(() => {
       return selectedOptions;
@@ -76,21 +87,17 @@ const Select = ({
   }, [selectedOptions, handleOnClick]);
 
   return (
-    <StyledWrapper>
-      <StyledTopWrapper>
+    <StyledWrapper onMouseLeave={() => hideList()}>
+      <StyledTopWrapper onClick={(e: MouseEvent<HTMLDivElement>) => toggleList(e)}>
         {isSelectOpened ? (
           <StyledInput
             placeholder={inputPlaceholder}
-            handleOnChange={(e: React.ChangeEvent<HTMLInputElement>) => handleOnChange(e)}
+            handleOnChange={(e: React.ChangeEvent<HTMLInputElement>) => filterOptions(e)}
           />
         ) : (
           <StyledSelectCaption id="selectCaption">{selectCaption}</StyledSelectCaption>
         )}
-        <StyledArrowImage
-          onClick={() => setIsSelectOpened(!isSelectOpened)}
-          isSelectOpened={isSelectOpened}
-          src={ArrowIcon}
-        />
+        <StyledArrowImage isSelectOpened={isSelectOpened} src={ArrowIcon} />
       </StyledTopWrapper>
       {isSelectOpened && (
         <StyledSelect aria-multiselectable={isMulti} aria-labelledby="selectCaption" role="listbox">
