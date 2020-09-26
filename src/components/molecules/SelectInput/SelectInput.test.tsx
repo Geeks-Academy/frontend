a010b700d6a8da72c29d251b9c4f29bf9a544ae1
Tomasz Dłuski <mr.toumash@gmail.com>
2020-09-26 11:51:34 +0200

    FRDQ-50 - Adds tests for SelectInput

---
+++ src/components/molecules/SelectInput/SelectInput.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import SelectInput from './SelectInput';
+
+describe('SelectInput', () => {
+  test('render default SelectInput component', () => {
+    render(
+      <SelectInput
+        inputPlaceholder="inputplaceholder"
+        selectCaption=""
+        options={[{ id: '0', value: 'Option 1' }]}
+        onClick={() => {}}
+      />
+    );
+  });
+  test('fires OnClick event on selection change', () => {
+    const mock = jest.fn();
+    const option1 = { id: '0', value: 'Option 1' };
+    const { getByText } = render(
+      <SelectInput
+        inputPlaceholder="inputplaceholder"
+        selectCaption="selectCaption"
+        options={[option1]}
+        onClick={mock}
+      />
+    );
+
+    // open the select dropdown
+    fireEvent.click(getByText('selectCaption'));
+    // select some option
+    fireEvent.click(getByText(option1.value));
+
+    expect(mock).toHaveBeenLastCalledWith([option1]);
+  });
+});
