9201bf788fc8b3dabdddda9214fcfd08afdb6a09
Tomasz Korenberg <info@casestudio.pl>
2020-09-27 12:26:15 +0200

    FRDQ-75 - Spinner storybook

---
+++ src/components/atoms/Spinner/Spinner.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Spinner from './Spinner';
+
+export default {
+  title: 'atoms/Spinner',
+  component: Spinner,
+};
+
+export const SpinnerComponent = (): JSX.Element => <Spinner />;
