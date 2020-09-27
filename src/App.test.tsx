308ad95dcaea4e63145eb735589c4bbadf216fa2
Hayuna <marek.kowalonek@gmail.com>
2020-09-27 12:08:17 +0200

    FRDQ-115 - Refactor redux setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import typography from 'styles/typography';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'store';
 import App from './App';
 
 afterEach(cleanup);
@@ -9,9 +11,11 @@ afterEach(cleanup);
 describe('App', () => {
   test('render App must include GlobalStyles', () => {
     const { container } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
     expect(container).toHaveStyle(typography.globalStyles);
   });
