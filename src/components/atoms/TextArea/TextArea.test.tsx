15098657939a8aed4e16ca97a01f73e63f19438e
Tomasz Korenberg <info@casestudio.pl>
2020-09-29 10:53:19 +0200

    FRDQ-93 - TextArea component tests

--- src/components/atoms/TextArea/TextArea.test.tsx
+++ src/components/atoms/TextArea/TextArea.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import TextArea from 'components/atoms/TextArea';
-import { cleanup, render } from '@testing-library/react';
-
-afterEach(cleanup);
+import { fireEvent, render } from '@testing-library/react';
 
 describe('TextArea', () => {
+  const setup = () => {
+    const utils = render(<TextArea />);
+    const input = utils.getByTestId('textarea');
+    return {
+      input,
+      ...utils,
+    };
+  };
+
   test(' render TextArea component', () => {
     const { getByTestId } = render(<TextArea />);
     expect(getByTestId('textarea')).toBeInTheDocument();
@@ -13,5 +20,11 @@ describe('TextArea', () => {
     const placeholder = 'Type something...';
     const { getByTestId } = render(<TextArea placeholder={placeholder} />);
     expect(getByTestId('textarea')).toBeInTheDocument();
+  });
+  test('TextArea onChange check', () => {
+    const { input } = setup();
+    const testValue = 'test value';
+    fireEvent.change(input, { target: { value: testValue } });
+    expect((input as HTMLInputElement).value).toBe(testValue);
   });
 });
