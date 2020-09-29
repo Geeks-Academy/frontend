75e8e5655fe846370834cf226f5f60c12db6e69c
Marcin Krysiński <marcin.krysik11@gmail.com>
2020-09-29 20:13:26 +0200

    FRDQ-118 - Change txt prop to children, update test file for paragraph.

--- src/components/atoms/Paragraph/Paragraph.test.tsx
+++ src/components/atoms/Paragraph/Paragraph.test.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Paragraph from './Paragraph';
-
-afterEach(cleanup);
 
 describe('Paragraph', () => {
   test('render Paragraph component with text', () => {
     const text = 'Test text';
-    const { getByText } = render(<Paragraph txt={text} />);
+    const { getByText } = render(<Paragraph>{text}</Paragraph>);
     expect(getByText(text)).toBeInTheDocument();
   });
 });
