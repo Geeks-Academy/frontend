bb2123a000c3e0ed2f196a018bee655bb97a0903
Hayuna <marek.kowalonek@gmail.com>
2020-10-13 19:20:08 +0200

    FRDQ-78 - Fix typing

--- src/Routes/helpers.tsx
+++ src/Routes/helpers.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
+interface LocationState {
+  from: {
+    pathname: string;
+  };
+}
+
 export const fakeAuth = {
   isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
   authenticate(cb: () => void): void {
@@ -12,9 +18,9 @@ export const fakeAuth = {
 
 export const LoginPage = (): JSX.Element => {
   const history = useHistory();
-  const location = useLocation();
+  const location = useLocation<LocationState>();
   // remember to do this at login page. If you click the back button at this point you don't go back to login page
-  const { from }: any = location.state || { from: { pathname: '/' } };
+  const { from } = location.state || { from: { pathname: '/' } };
   const login = () => {
     fakeAuth.authenticate(() => {
       history.replace(from);
