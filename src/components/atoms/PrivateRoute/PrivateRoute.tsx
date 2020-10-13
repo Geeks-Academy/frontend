d70d56f61c5dc614b6d72b6e2c34cd97ad422d0a
Tomasz Korenberg <info@casestudio.pl>
2020-10-13 16:47:26 +0200

    FRDQ-78 - Add protected route functionality - fixed: redirect to good path after login

--- src/components/atoms/PrivateRoute/PrivateRoute.tsx
+++ src/components/atoms/PrivateRoute/PrivateRoute.tsx
@@ -3,10 +3,23 @@ import React from 'react';
 import { fakeAuth } from 'Routes/helpers';
 
 const PrivateRoute = ({ children, ...rest }: RouteProps): JSX.Element => {
+  const location = useLocation();
+
   return (
     <Route
       {...rest}
-      render={() => (fakeAuth.isAuthenticated ? children : <Redirect to="/login" />)}
+      render={() =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
     />
   );
 };
