873b303074f2f9df7bed4bae13294ffb4637bc9e
Tomasz Korenberg <info@casestudio.pl>
2020-10-13 07:05:57 +0200

    FRDQ-78 - Add protected route functionality - Added localStorage to fakeAuth

--- src/Routes/helpers.tsx
+++ src/Routes/helpers.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 export const fakeAuth = {
-  isAuthenticated: false,
+  isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
   authenticate(cb: () => void): void {
-    fakeAuth.isAuthenticated = true;
+    localStorage.setItem('isAuthenticated', 'true');
+    this.isAuthenticated = true;
     setTimeout(cb, 1000);
   },
 };
@@ -12,8 +13,7 @@ export const fakeAuth = {
 export const LoginPage = (): JSX.Element => {
   const history = useHistory();
   const location = useLocation();
-  // remember to do this at login page. If you click the back button at this point you don't go back to login page because
-  // you are looged in
+  // remember to do this at login page. If you click the back button at this point you don't go back to login page
   const { from }: any = location.state || { from: { pathname: '/' } };
   const login = () => {
     fakeAuth.authenticate(() => {
