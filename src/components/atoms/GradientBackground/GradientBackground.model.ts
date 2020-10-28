e49fcd16c013e1095c33d54af6ce4d2e763758ff
Marcin Krysiński <marcin.krysik11@gmail.com>
2020-10-28 15:03:34 +0100

    FRDQ-144 - Update model for GradientBackground component.

--- src/components/atoms/GradientBackground/GradientBackground.model.ts
+++ src/components/atoms/GradientBackground/GradientBackground.model.ts
@@ -5,7 +5,7 @@ export type GradientType = {
 
 export interface IProps {
   background: string;
-  children?: JSX.Element[] | JSX.Element;
+  children?: JSX.Element[] | JSX.Element | string;
   className?: string;
   gradient?: GradientType;
 }
