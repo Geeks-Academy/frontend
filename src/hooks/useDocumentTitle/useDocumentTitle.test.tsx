2d2128bcca8ad9177dcb0ac2248088cb4ae46ecb
piotrkosinski1992@o2.pl <piotrkosinski1992@o2.pl>
2020-11-01 22:55:14 +0100

    FRDQ-128 - added export inside of index.ts for userDocumentTitle hook

--- src/hooks/useDocumentTitle/useDocumentTitle.test.tsx
+++ src/hooks/useDocumentTitle/useDocumentTitle.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import useDocumentTitle from './useDocumentTitle';
+import { useDocumentTitle } from 'hooks';
 
 const Component = (): JSX.Element => {
   useDocumentTitle('Document Title');
