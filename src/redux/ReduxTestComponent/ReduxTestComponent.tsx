0eafe0ecea8470cb4c98fa7e986a0b5247332470
olgaz <olgaz@nask.pl>
2020-11-05 23:39:07 +0100

    FRDQ-124 - Add example of multistep component

--- src/redux/ReduxTestComponent/ReduxTestComponent.tsx
+++ src/redux/ReduxTestComponent/ReduxTestComponent.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import loader from 'redux/reducers/loader';
 import { RootState, store } from 'redux/store';
 import { useSelector } from 'react-redux';
-import Spinner from 'components/atoms/Spinner';
+import Spinner from '../../components/atoms/Spinner';
+import Input from '../../components/atoms/Input';
+import MultiStepForm from '../../components/molecules/MultiStepForm/MultiStepForm';
 
 const ReduxTestComponent = (): JSX.Element => {
   const showLoader = useSelector((state: RootState) => state.loader.isLoading);
@@ -13,6 +15,13 @@ const ReduxTestComponent = (): JSX.Element => {
       <button type="button" onClick={() => store.dispatch(toggleLoader())}>
         {!showLoader ? 'Show loader' : 'Hide Loader'}
       </button>
+      <MultiStepForm>
+        <div>
+          <Input placeholder="surname" label="Surname" />
+          <Input placeholder="name" label="Name" />
+        </div>
+        <Input placeholder="age" label="Age" />
+      </MultiStepForm>
     </>
   );
 };
