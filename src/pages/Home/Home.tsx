9c67340191ac1703337ba3d9fb1d4f985dd2c876
Hayuna <marek.kowalonek@gmail.com>
2020-12-08 16:46:40 +0100

    FRDQ-9999 - First try to communicate front-back

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,23 +15,31 @@ import {
 
 // "StyledWrapper - div" and "main" tags in future can use in template and remove from here.
 
-const Home = (): JSX.Element => (
-  <StyledWrapper>
-    <main>
-      <StyledInnerWrapper>
-        <StyledContainer>
-          <StyledHeading>{data.header}</StyledHeading>
-          <StyledList icon={listItemIcon} items={data.listDescription} />
-          <StyledParagraph>{data.paragraph}</StyledParagraph>
-          <StyledButtonsWrapper>
-            <SocialButton logo="github" userRole="developer" />
-            <SocialButton logo="google" userRole="non-developer" />
-          </StyledButtonsWrapper>
-        </StyledContainer>
-        <StyledHomeImg src={HomePageImg_1} alt="Register" />
-      </StyledInnerWrapper>
-    </main>
-  </StyledWrapper>
-);
+const Home = (): JSX.Element => {
+  const handleClick = async () => {
+    const a = await fetch('http://auth.programmers.only/auth/github');
+    const b = await a.json();
+    console.log(b);
+  };
+
+  return (
+    <StyledWrapper>
+      <main>
+        <StyledInnerWrapper>
+          <StyledContainer>
+            <StyledHeading>{data.header}</StyledHeading>
+            <StyledList icon={listItemIcon} items={data.listDescription} />
+            <StyledParagraph>{data.paragraph}</StyledParagraph>
+            <StyledButtonsWrapper>
+              <SocialButton logo="github" userRole="developer" onClick={handleClick} />
+              <SocialButton logo="google" userRole="non-developer" onClick={handleClick} />
+            </StyledButtonsWrapper>
+          </StyledContainer>
+          <StyledHomeImg src={HomePageImg_1} alt="Register" />
+        </StyledInnerWrapper>
+      </main>
+    </StyledWrapper>
+  );
+};
 
 export default Home;
