4b34660e938c28bfe57481384edb1137e3e521f3
Hayuna <marek.kowalonek@gmail.com>
2020-12-08 17:54:36 +0100

    FRDQ-9999 - First try to communicate front-back

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -17,15 +17,6 @@ import {
 
 const Home = (): JSX.Element => {
   const handleClick = async () => {
-    // const a1 = await fetch('auth.programmers.only/auth/github');
-    // const b1 = await a1.json();
-    // console.log(b1);
-    const a2 = await fetch('/auth.programmers.only/auth/github');
-    const b2 = await a2.json();
-    console.log(b2);
-    // const a3 = await fetch('https://auth.programmers.only/auth/github');
-    // const b3 = await a3.json();
-    // console.log(b3);
     const a4 = await fetch('http://auth.programmers.only/auth/github');
     const b4 = await a4.json();
     console.log(b4);
