8c80356c32e14451ae7c9933fdabc1d56136e361
PiotrGrobelak <grobelak.piotr@gmail.com>
2021-01-10 21:28:31 +0100

    FRDQ-169 - Add commetns about document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,13 @@
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+//  _document.js/tsx is a root file of our application
+// in this file we can add/modify our static head, main and script tags
+// probably in the future, this file will be larger
+// we can't write some logic, include some layouts or use events like "onClick"
+// https://nextjs.org/docs/advanced-features/custom-document
+
+
 // SC feature with next.js
 // serve requierd styles for first redner and then load rest styles for the app
 
