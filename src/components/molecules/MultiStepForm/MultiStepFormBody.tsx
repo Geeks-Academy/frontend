5de96553410441181dde8c76899f3f29f1ed68af
Konrad Rudnicki <zeglarz@gmail.com>
2021-02-11 23:54:34 +0100

    FRDQ-157 - MultiStepForm progress fix bug with progress starting from 0

--- src/components/molecules/MultiStepForm/MultiStepFormBody.tsx
+++ src/components/molecules/MultiStepForm/MultiStepFormBody.tsx
@@ -21,7 +21,7 @@ const MultiStepFormBody = ({
 
   const lastStep = React.Children.count(children) - 1;
   const steps = React.Children.toArray(children);
-  const amountOfSteps = Array.from(Array(steps.length).keys());
+  const amountOfSteps = [...Array(steps.length)].map((v, i) => i + 1);
 
   const formData = watch();
 
