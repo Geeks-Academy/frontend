cd9cd86a7bd09a1a772da86c7cc722e87ec9dd9e
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2021-04-20 08:17:14 +0200

    FRDQ-179 - Implemented providers in progress

---
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+import LoginPage from 'pages/LoginPage';
+import { FC, createContext } from 'react';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+interface AuthContextValue {
+  user: any; // Temporary any
+  logOut: () => void;
+}
+
+export const AuthContext = createContext<AuthContextValue>({
+  user: null,
+  logOut: () => () => null,
+});
+
+export const AuthProvider: FC<AuthProviderProps> = ({ children }): JSX.Element => {
+  const [session] = useSession();
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: session?.user,
+        logOut: signOut,
+      }}
+    >
+      {session?.user ? children : <LoginPage signIn={signIn} />}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,3 @@
+import LoginPage from './TemporaryLoginPage';
+
+export default LoginPage;
